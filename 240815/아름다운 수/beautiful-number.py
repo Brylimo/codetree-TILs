43267747d279facf12bf649097cdef1be8233453
Chaejin <icj0103@gmail.com>
2024-08-15 10:36:30 +0900

    Title: 아름다운 수, Time: 393ms, Memory: 42MB, Status: Passed - CodeTree

--- 240815/아름다운 수/beautiful-number.py
+++ 240815/아름다운 수/beautiful-number.py
@@ -1,19 +1,22 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input().rstrip())
 
 cnt = 0
 answer = []
+
+
 def choose(curr_num):
     global cnt
-    
+
     if curr_num == n + 1:
         digit_cnt = 1
         flag = False
 
-        for i in range(len(answer) - 1):
-            if len(answer) > 1 and answer[i] == answer[i + 1]:
+        for i in range(len(answer)):
+            if len(answer) > 1 and i < len(answer) - 1 and answer[i] == answer[i + 1]:
                 digit_cnt += 1
                 continue
             else:
@@ -21,11 +24,7 @@ def choose(curr_num):
                     flag = True
                     break
 
-                digit_cnt = 0
-
-        if len(answer) == 1:
-            if digit_cnt < answer[0] or digit_cnt % answer[0] != 0:
-                flag = True
+                digit_cnt = 1
 
         if not flag:
             cnt += 1
@@ -38,6 +37,8 @@ def choose(curr_num):
         answer.pop()
 
     return
+
+
 choose(1)
 
 print(cnt)
