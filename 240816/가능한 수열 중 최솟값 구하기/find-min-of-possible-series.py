2c45726b364bd64057ff0382d31336504d7ff7a1
Chaejin <icj0103@gmail.com>
2024-08-16 19:48:08 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 1699ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240816/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+sequence = []
+
+def is_possible():
+    for k in range(1, n // 2 + 1):
+        for j in range(0, n - k, k):
+            if str(sequence[j:j+k]) == str(sequence[j+k:j+2*k]):
+                return False
+
+    return True
+
+result = []
+def calc(curr_num):
+    if curr_num == n:
+        if is_possible():
+            result.append(tuple(sequence))
+        return
+
+    for i in range(4, 7):
+        sequence.append(i)
+        calc(curr_num + 1)
+        sequence.pop()
+
+    return
+
+calc(0)
+result.sort()
+for i in range(len(result[0])):
+    print(result[0][i], end="")
