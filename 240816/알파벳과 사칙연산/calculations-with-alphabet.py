95a0bfd053a011cd114138f0fa21bd0c42858dde
Chaejin <icj0103@gmail.com>
2024-08-16 11:36:00 +0900

    Title: 알파벳과 사칙연산, Time: 151ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240816/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+string = input().rstrip()
+line = list(string)
+
+op_list = string.replace('+', '-').replace('*', '-').split('-')
+
+array = []
+
+def evaluate():
+    idx = 0
+    prev = 0
+    opp = None
+    for op in line:
+        if ord('a') <= ord(op) <= ord('f'):
+            if opp is None:
+                prev += array[idx]
+            elif opp == '-':
+                prev -= array[idx]
+            elif opp == '*':
+                prev *= array[idx]
+            else:
+                prev += array[idx]
+            idx += 1
+        else:
+            opp = op
+
+    return prev
+
+max_val = 0
+def calc(curr_num):
+    global max_val
+    if curr_num == len(op_list):
+        res = evaluate()
+        max_val = max(max_val, res)
+        return
+
+    for i in range(1, 5):
+        array.append(i)
+        calc(curr_num + 1)
+        array.pop()
+
+calc(0)
+
+print(max_val)
