edcc37af93d7c4ef82f0ba820892c664afa11451
Chaejin <icj0103@gmail.com>
2024-08-17 15:33:51 +0900

    Title: 외판원 순회, Time: 1409ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240817/외판원 순회/traveling-salesman-problem.py
+++ 240817/외판원 순회/traveling-salesman-problem.py
@@ -10,17 +10,32 @@ answer = []
 visited = [False] * n
 
 min_val = sys.maxsize
+def is_possible():
+    for tx, ty in answer:
+        if (ty, tx) in answer:
+            return False
+
+    return True
+
+def calc_sum():
+    t_sum = 0
+    for tx, ty in answer:
+        t_sum += graph[tx][ty]
+    return t_sum
+
 def calc(curr_num):
     global min_val
     if curr_num == n:
-        min_val = min(min_val, sum(answer))
+        if is_possible():
+            min_val = min(min_val, calc_sum())
+            return
         return
 
     for i in range(n):
-        if visited[i] or curr_num == i:
+        if visited[i] or curr_num == i or graph[curr_num][i] == 0:
             continue
 
-        answer.append(graph[curr_num][i])
+        answer.append((curr_num, i))
         visited[i] = True
         calc(curr_num + 1)
         visited[i] = False
