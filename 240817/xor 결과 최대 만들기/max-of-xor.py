659f3495ee0f43414970a23971bb2a5c18104b78
Chaejin <icj0103@gmail.com>
2024-08-17 10:25:12 +0900

    Title: xor 결과 최대 만들기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240817/xor 결과 최대 만들기/max-of-xor.py
+++ 240817/xor 결과 최대 만들기/max-of-xor.py
@@ -8,15 +8,15 @@ max_val = 0
 combinations = []
 
 def xor():
-    result = 0
-    for num in combinations:
-        result = result ^ num
+    result = combinations[0]
+    for i in range(1, len(combinations)):
+        result = result ^ combinations[i]
 
     return result
 
 def calc(curr_num, cnt):
     global max_val
-    if curr_num == n + 1:
+    if curr_num == n:
         if cnt == m:
             max_val = max(max_val, xor())
         return
@@ -27,5 +27,5 @@ def calc(curr_num, cnt):
 
     calc(curr_num + 1, cnt)
 
-calc(1, 0)
+calc(0, 0)
 print(max_val)
