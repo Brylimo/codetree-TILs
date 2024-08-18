4e316039741e9b0e6111ad56fdcb45e3bf62cb1b
Chaejin <icj0103@gmail.com>
2024-08-18 10:24:28 +0900

    Title: Date to Date, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240818/Date to Date/date-to-date.py
+++ 240818/Date to Date/date-to-date.py
@@ -3,12 +3,12 @@ m1, d1, m2, d2 = map(int, input().rstrip().split())
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 time1 = 0
 time2 = 0
-for i in range(1, m1 + 1):
+for i in range(1, m1):
     time1 += num_of_days[i]
 time1 += d1
 
-for i in range(1, m2 + 1):
+for i in range(1, m2):
     time2 += num_of_days[i]
 time2 += d2
 
-print(time2 - time1)
+print(time2 - time1 + 1)
