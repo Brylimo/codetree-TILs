319862f70745b574894a78e171d827af9a1b978b
Chaejin <icj0103@gmail.com>
2024-08-19 20:47:09 +0900

    Title: 좌우로 움직이는 로봇, Time: 175ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240819/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240819/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -15,6 +15,9 @@ for _ in range(n):
             pos_n[tick] = pos_n[tick - 1] + 1 
             tick += 1
 
+for i in range(tick, 1000001):
+    pos_n[tick] = pos_n[tick - 1]
+
 pos_m = [0] * 1000001
 tick = 1
 for _ in range(m):
@@ -30,8 +33,12 @@ for _ in range(m):
             pos_m[tick] = pos_m[tick - 1] + 1
             tick += 1
 
+for i in range(tick, 1000001):
+    pos_m[tick] = pos_m[tick - 1]
+
 cnt = 0
 max_tick = max(len(pos_n), len(pos_m))
+
 for i in range(1, max_tick):
     if i > 1 and pos_n[i] == pos_m[i] and pos_m[i - 1] != pos_n[i - 1]:
         cnt += 1
