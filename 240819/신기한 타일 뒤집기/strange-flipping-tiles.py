00d4244b385bc8392013a67015bf2548f83cd44e
Chaejin <icj0103@gmail.com>
2024-08-19 14:40:39 +0900

    Title: 신기한 타일 뒤집기, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240819/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240819/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -11,19 +11,19 @@ for _ in range(n):
     if direction == 'R':
         color = 'b'
         segment_left = cur
-        segment_right = cur + distance
-        cur += distance
+        segment_right = cur + distance - 1
+        cur += distance - 1
     elif direction == 'L':
         color = 'w'
-        segment_left = cur - distance
+        segment_left = cur - distance + 1
         segment_right = cur
-        cur -= distance
+        cur -= distance + 1
 
     segments.append((color, segment_left, segment_right))
 
 line = [0] * MAX_R
 for color, segment_left, segment_right in segments:
-    for i in range(OFFSET + segment_left, OFFSET + segment_right):
+    for i in range(OFFSET + segment_left, OFFSET + segment_right + 1):
         if color == 'w':
             line[i] = 1
         elif color == 'b':
