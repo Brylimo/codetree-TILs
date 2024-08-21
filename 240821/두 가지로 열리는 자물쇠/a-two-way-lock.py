14de0d9683e805c022be3937b73de2141a30d2a1
Chaejin <icj0103@gmail.com>
2024-08-21 20:18:01 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 158ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240821/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240821/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -3,7 +3,7 @@ a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
 feasible_a1 = [a1, (1 if (a1 + 1) % (n+1) == 0 else (a1 + 1) % (n+1)), (1 if (a1 + 2) % (n+1) == 0 else (a1 + 2) % (n+1)), (n + a1 - 1 if a1 - 1 <= 0 else a1 - 1), (n + a1 - 2 if a1 - 2 <= 0 else a1 - 2)]
-feasible_b1 = [b1, (1 if (b1 + 1) % (n+1) == 0 else (b1 + 1) % (n+1)), (1 if (b1 + 2) % (n+1) == 0 else (b1 + 1) % (n+1)), (n + b1 - 1 if b1 - 1 <= 0 else b1 - 1), (n + b1 - 2 if b1 - 2 <= 0 else b1 - 2)]
+feasible_b1 = [b1, (1 if (b1 + 1) % (n+1) == 0 else (b1 + 1) % (n+1)), (1 if (b1 + 2) % (n+1) == 0 else (b1 + 2) % (n+1)), (n + b1 - 1 if b1 - 1 <= 0 else b1 - 1), (n + b1 - 2 if b1 - 2 <= 0 else b1 - 2)]
 feasible_c1 = [c1, (1 if (c1 + 1) % (n+1) == 0 else (c1 + 1) % (n+1)), (1 if (c1 + 2) % (n+1) == 0 else (c1 + 2) % (n+1)), (n + c1 - 1 if c1 - 1 <= 0 else c1 - 1), (n + c1 - 2 if c1 - 2 <= 0 else c1 - 2)]
 
 feasible_a2 = [a2, (1 if (a2 + 1) % (n+1) == 0 else (a2 + 1) % (n+1)), (1 if (a2 + 2) % (n+1) == 0 else (a2 + 2) % (n+1)), (n + a2 - 1 if a2 - 1 <= 0 else a2 - 1), (n + a2 - 2 if a2 - 2 <= 0 else a2 - 2)]
