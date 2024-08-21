c010c9d76ce9c61251270a8df84cc263a53ee2f7
Chaejin <icj0103@gmail.com>
2024-08-21 20:26:02 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 234ms, Memory: 26MB, Status: Passed - CodeTree

--- 240821/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240821/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,20 +1,22 @@
+# 변수 선언 및 입력
 n = int(input())
-a1, b1, c1 = map(int, input().split())
-a2, b2, c2 = map(int, input().split())
+a, b, c = tuple(map(int, input().split()))
+a2, b2, c2 = tuple(map(int, input().split()))
 
-feasible_a1 = [a1, (1 if (a1 + 1) % (n+1) == 0 else (a1 + 1) % (n+1)), (1 if (a1 + 2) % (n+1) == 0 else (a1 + 2) % (n+1)), (n + a1 - 1 if a1 - 1 <= 0 else a1 - 1), (n + a1 - 2 if a1 - 2 <= 0 else a1 - 2)]
-feasible_b1 = [b1, (1 if (b1 + 1) % (n+1) == 0 else (b1 + 1) % (n+1)), (1 if (b1 + 2) % (n+1) == 0 else (b1 + 2) % (n+1)), (n + b1 - 1 if b1 - 1 <= 0 else b1 - 1), (n + b1 - 2 if b1 - 2 <= 0 else b1 - 2)]
-feasible_c1 = [c1, (1 if (c1 + 1) % (n+1) == 0 else (c1 + 1) % (n+1)), (1 if (c1 + 2) % (n+1) == 0 else (c1 + 2) % (n+1)), (n + c1 - 1 if c1 - 1 <= 0 else c1 - 1), (n + c1 - 2 if c1 - 2 <= 0 else c1 - 2)]
 
-feasible_a2 = [a2, (1 if (a2 + 1) % (n+1) == 0 else (a2 + 1) % (n+1)), (1 if (a2 + 2) % (n+1) == 0 else (a2 + 2) % (n+1)), (n + a2 - 1 if a2 - 1 <= 0 else a2 - 1), (n + a2 - 2 if a2 - 2 <= 0 else a2 - 2)]
-feasible_b2 = [b2, (1 if (b2 + 1) % (n+1) == 0 else (b2 + 1) % (n+1)), (1 if (b2 + 2) % (n+1) == 0 else (b2 + 2) % (n+1)), (n + b2 - 1 if b2 - 1 <= 0 else b2 - 1), (n + b2 - 2 if b2 - 2 <= 0 else b2 - 2)]
-feasible_c2 = [c2, (1 if (c2 + 1) % (n+1) == 0 else (c2 + 1) % (n+1)), (1 if (c2 + 2) % (n+1) == 0 else (c2 + 2) % (n+1)), (n + c2 - 1 if c2 - 1 <= 0 else c2 - 1), (n + c2 - 2 if c2 - 2 <= 0 else c2 - 2)]
-
+# 모든 조합을 다 만들어 봅니다.
 cnt = 0
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         for k in range(1, n + 1):
-            if (i in feasible_a1 and j in feasible_b1 and k in feasible_c1) or (i in feasible_a2 and j in feasible_b2 and k in feasible_c2):
+            # 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+            if (abs(a - i) <= 2 or abs(a - i) >= n - 2) and (abs(b - j) <= 2 or abs(b - j) >= n - 2) and \
+               (abs(c - k) <= 2 or abs(c - k) >= n - 2):
                 cnt += 1
-                
+			
+			# 모든 자리가 주어진 두 번째 조합과의 거리가 2 이내인지 확인합니다.
+            elif (abs(a2 - i) <= 2 or abs(a2 - i) >= n - 2) and (abs(b2 - j) <= 2 or abs(b2 - j) >= n - 2) and \
+               (abs(c2 - k) <= 2 or abs(c2 - k) >= n - 2):
+                cnt += 1
+
 print(cnt)
