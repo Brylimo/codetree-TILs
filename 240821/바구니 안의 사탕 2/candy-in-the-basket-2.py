f19eaa0fba3ca4e3bda830ce360535934bb2a32e
Chaejin <icj0103@gmail.com>
2024-08-21 15:55:22 +0900

    Title: 바구니 안의 사탕 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240821/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240821/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -6,9 +6,12 @@ for _ in range(n):
     array[pos] += candy
 
 ans = 0
-for i in range(k + 1, 101 - k - 1):
+for i in range(1, 101):
+    left = (1 if i - k < 0 else i - k)
+    right = (101 if i + k >= 100 else i + k + 1)
+
     sum_interval = 0
-    for j in range(i - k, i + k + 1):
+    for j in range(left, right):
         sum_interval += array[j]
     ans = max(ans, sum_interval)
 
