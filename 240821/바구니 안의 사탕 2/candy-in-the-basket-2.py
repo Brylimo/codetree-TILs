8808ce340c3eaa576bcdaf881c42c6a6492d8a1c
Chaejin <icj0103@gmail.com>
2024-08-21 15:36:14 +0900

    Title: 바구니 안의 사탕 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240821/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240821/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
 
 array = [0] * 101
-for _ in range(4):
+for _ in range(n):
     candy, pos = map(int, input().split())
     array[pos] += candy
 
 ans = 0
-for i in range(k, 101 - k):
+for i in range(k + 1, 101 - k - 1):
     sum_interval = 0
     for j in range(i - k, i + k + 1):
         sum_interval += array[j]
