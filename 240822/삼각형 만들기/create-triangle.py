2d58724228a1d90c7717e9b56a1a2318fe36ca5b
Chaejin <icj0103@gmail.com>
2024-08-22 13:51:29 +0900

    Title: 삼각형 만들기, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240822/삼각형 만들기/create-triangle.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+
+points= []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            one = points[i]
+            two = points[j]
+            three = points[k]
+
+            min_x = min(one[0], two[0], three[0])
+            max_x = max(one[0], two[0], three[0])
+            min_y = min(one[1], two[1], three[1])
+            max_y = max(one[1], two[1], three[1])
+
+            ans = max(ans, (max_x - min_x) * (max_y - min_y))
+
+print(ans)
