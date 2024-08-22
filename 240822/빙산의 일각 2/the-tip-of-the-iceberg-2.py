e0ad6c571ac10bc964d9de3ebb8143fc6cec6d8b
Chaejin <icj0103@gmail.com>
2024-08-22 21:57:23 +0900

    Title: 빙산의 일각 2, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240822/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -6,10 +6,15 @@ for _ in range(n):
 
 ans = 0
 for i in range(1, 1001):
-    cnt = 1
-    for j in range(len(h) - 1):
-        if i < h[j] and i >= h[j + 1]:
+    flag = False
+    cnt = 0
+    for j in range(len(h)):
+        if flag == False and i < h[j]:
+            flag = True
             cnt += 1
+        
+        if flag and i >= h[j]:
+            flag = False
 
     ans = max(ans, cnt)
 
