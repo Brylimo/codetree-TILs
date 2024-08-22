2d057f56cc668a180a281831bb20c3bc357504f8
Chaejin <icj0103@gmail.com>
2024-08-22 19:51:06 +0900

    Title: 스승의 은혜 3, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240822/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240822/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -10,7 +10,7 @@ for _ in range(n):
     p, s = map(int, input().split())
     presents.append(Present(p, s))
 
-presents.sort(key=lambda a:a.p)
+presents.sort(key=lambda a:(a.p + a.s))
 
 ans = 0
 for i in range(n):
@@ -18,7 +18,7 @@ for i in range(n):
     sum_value = 0
     for j in range(n):
         if i == j:
-            sum_value += presents[j].p // 2 + presents[j].s
+            sum_value += (presents[j].p + presents[j].s) // 2
             if sum_value > b:
                 break
             cnt += 1
