2effac9cc83d6a51f2c3218d046c1306a7db1c69
Chaejin <icj0103@gmail.com>
2024-08-23 09:13:09 +0900

    Title: 야바위, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/야바위/ya-rock.py
+++ 240823/야바위/ya-rock.py
@@ -5,12 +5,11 @@ for _ in range(n):
     a, b, c = map(int, input().split())
     games.append((a, b, c))
 
-ans = 1
 total = 0
-score = 0
 for i in range(1, 4):    
     stones = [0] * 4
     stones[i] = 1
+    score = 0
     for j in range(n):
         a, b, c = games[j]
 
@@ -21,6 +20,5 @@ for i in range(1, 4):
 
     if total < score:
         total = score
-        ans = i 
 
-print(ans)
+print(total)
