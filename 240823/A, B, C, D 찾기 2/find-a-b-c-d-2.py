f42f4ca3c34819cadad6e673044945fc46bb3307
Chaejin <icj0103@gmail.com>
2024-08-23 20:58:03 +0900

    Title: A, B, C, D 찾기 2, Time: 383ms, Memory: 26MB, Status: Passed - CodeTree

--- 240823/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240823/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import sys
 
 array = list(map(int, input().split()))
@@ -8,50 +9,66 @@ for i in range(1, 41):
             for l in range(k, 41):
                 a, b, c, d = i, j, k, l
 
-                if a not in array:
+                varray = deepcopy(array)
+                if a not in varray:
                     continue
                 
-                if b not in array:
+                varray.remove(a)
+                if b not in varray:
+                    continue
+                    
+                varray.remove(b)
+                if c not in varray:
                     continue
 
-                if c not in array:
+                varray.remove(c)
+                if d not in varray:
                     continue
 
-                if d not in array:
+                varray.remove(d)
+                if a + b not in varray:
                     continue
 
-                if a + b not in array:
-                    continue
-
-                if b + c not in array:
+                varray.remove(a + b)
+                if b + c not in varray:
                     continue
                 
-                if c + d not in array:
+                varray.remove(b + c)
+                if c + d not in varray:
                     continue
 
-                if d + a not in array:
+                varray.remove(c + d)
+                if d + a not in varray:
                     continue
 
-                if a + c not in array:
+                varray.remove(d + a)
+                if a + c not in varray:
                     continue
 
-                if b + d not in array:
+                varray.remove(a + c)
+                if b + d not in varray:
                     continue
 
-                if a + b + c not in array:
+                varray.remove(b + d)
+                if a + b + c not in varray:
                     continue
 
-                if a + b + d not in array:
+                varray.remove(a + b + c)
+                if a + b + d not in varray:
                     continue
 
-                if a + c + d not in array:
+                varray.remove(a + b + d)
+                if a + c + d not in varray:
                     continue
 
-                if b + c + d not in array:
+                varray.remove(a + c + d)
+                if b + c + d not in varray:
                     continue
 
-                if a + b + c + d not in array:
+                varray.remove(b + c + d)
+                if a + b + c + d not in varray:
                     continue
 
+                varray.remove(a + b + c + d)
                 print(a, b, c, d)
                 sys.exit(0)
