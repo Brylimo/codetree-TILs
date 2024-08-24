40b45d93ec36cd05d83eb6ab41db00259e1235d5
Chaejin <icj0103@gmail.com>
2024-08-24 19:57:44 +0900

    Title: X 달리기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240824/X 달리기/x-run.py
+++ 240824/X 달리기/x-run.py
@@ -4,6 +4,7 @@ half = x // 2
 velo = 1
 dist = 0
 ans = 0
+prev = 0
 for i in range(1, x + 1):
     dist += velo
 
@@ -11,12 +12,14 @@ for i in range(1, x + 1):
         ans = i
         break
 
-    if dist < half:
+    if dist + dist < x:
         velo += 1
-    elif dist == half:
+    elif dist + prev < x:
         pass
     else:
         if velo > 1:
-            velo -= 1    
+            velo -= 1
+
+    prev = dist    
     
 print(ans)
