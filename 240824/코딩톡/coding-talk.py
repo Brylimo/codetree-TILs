e7eca2c8d237ef46f53ae1dd836c7b97f98245cc
Chaejin <icj0103@gmail.com>
2024-08-24 17:23:22 +0900

    Title: 코딩톡, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240824/코딩톡/coding-talk.py
+++ 240824/코딩톡/coding-talk.py
@@ -1,6 +1,11 @@
 n, m, p = map(int, input().split())
 
 talks = []
+
+overall = []
+for i in range(n):
+    overall.append(chr(ord('A') + i))
+    
 for _ in range(m):
     c, u = input().split()
     u = int(u)
@@ -12,15 +17,17 @@ lasts = []
 for i in range(m, 0, -1):
     c, u = talks[i - 1]
 
+    if u == 0:
+        for alpha in overall:
+            people[i - 1].append(alpha)
+            lasts.append(alpha)
+            continue
+
     people[i - 1].append(c)
     for j in range(len(lasts)):
         people[i - 1].append(lasts[j])
 
     lasts.append(c)
-
-overall = []
-for i in range(n):
-    overall.append(chr(ord('A') + i))
 
 overall = set(overall)
 target = set(people[p - 1])
