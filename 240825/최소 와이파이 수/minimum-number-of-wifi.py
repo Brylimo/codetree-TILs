224a454bb4a380ea3645554a67e3782080f6353c
Chaejin <icj0103@gmail.com>
2024-08-25 17:57:26 +0900

    Title: 최소 와이파이 수, Time: 5093ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240825/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+
+ans = 0
+cnt = 0
+total = sum(array)
+while True:
+    loc = None
+    max_cnt = 0
+    block_size = 2*m + 1
+    for i in range(n - block_size + 1):
+        overlap = 0
+        for j in range(block_size):
+            if array[i + j] == 1:
+                overlap += 1
+
+        if max_cnt < overlap:
+            max_cnt = overlap
+            loc = i
+
+    cnt += max_cnt
+    if cnt != total:
+        ans += 1
+        if loc is not None:
+            for i in range(block_size):
+                array[loc + i] = 0
+    else:
+        ans += 1
+        break
+
+print(ans)
