0c2fb98a57717c017396711d143722654d104034
Chaejin <icj0103@gmail.com>
2024-08-27 16:49:24 +0900

    Title: 금 채굴하기, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240827/금 채굴하기/gold-mining.py
@@ -0,0 +1,53 @@
+from collections import deque
+n, m = map(int, input().split())
+
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+ans = 0
+def draw_diamond(x, y):
+    global ans
+
+    queue = deque([])
+    value = 0
+
+    visited = [[False] * n for _ in range(n)]
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    queue.append((x, y, 1))
+
+    visited[x][y] = True
+    value += grid[x][y]
+
+    cur_level = 0
+    cnt = 0
+    while queue:
+        ax, ay, level = queue.popleft()
+
+        cnt += 1
+        if level != cur_level:
+            cur_level = level
+            if cnt < value * m:
+                ans = max(ans, value)
+
+        for i in range(4):
+            nx = ax + dx[i]
+            ny = ay + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                break
+
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                value += grid[nx][ny]
+
+                if 2 * level + 1 <= n:
+                    queue.append((nx, ny, level + 1))
+        
+for i in range(n):
+    for j in range(n):
+        draw_diamond(i, j)
+
+print(ans)
