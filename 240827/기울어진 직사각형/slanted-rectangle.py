f8a77d5f53880156816101939c9ad1386cf9ebed
Chaejin <icj0103@gmail.com>
2024-08-27 20:09:24 +0900

    Title: 기울어진 직사각형, Time: 269ms, Memory: 26MB, Status: Passed - CodeTree

--- 240827/기울어진 직사각형/slanted-rectangle.py
+++ 240827/기울어진 직사각형/slanted-rectangle.py
@@ -1,47 +1,48 @@
-n = int(input())
+# 변수 선언 및 입력:
 
-grid = []
-for _ in range(n):
-    grid.append(list(map(int, input().split())))
+n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def get_score(x, y, k, l):
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+    move_nums = [k, l, k, l]
+    
+    sum_of_nums = 0
+
+    # 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x, y = x + dx, y + dy
+                
+            # 기울어진 직사각형이 경계를 벗어나는 경우라면
+            # 불가능하다는 의미로 답이 갱신되지 않도록
+            # 0을 반환합니다.
+            if not in_range(x, y):
+                return 0
+            
+            sum_of_nums += grid[x][y]
+    
+    return sum_of_nums
+
 
 ans = 0
-def draw_rect(x, y):
-    global ans
-    dx = [-1, -1, 1, 1]
-    dy = [1, -1, -1, 1]
 
-    check = [0, 0, 0, 0]
-    dir = 0
-
-    sum_val = 0
-    ax, ay = x, y
-
-    flag = True
-    while True:
-        nx = ax + dx[dir]
-        ny = ay + dy[dir]
-
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            if check[dir] == 0:
-                flag = False
-                break
-            dir += 1
-            if dir == 4:
-                break
-                
-            continue
-            
-        sum_val += grid[nx][ny]
-        check[dir] += 1
-
-        ax, ay = nx, ny
-
-    if flag:
-        flag = False
-        ans = max(ans, sum_val)
-
+# (i, j)를 시작으로 1, 2, 3, 4 방향
+# 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+# 기울어진 직사각형을 잡아보는
+# 완전탐색을 진행해봅니다.
 for i in range(n):
     for j in range(n):
-        draw_rect(i, j)
+        for k in range(1, n):
+            for l in range(1, n):
+                ans = max(ans, get_score(i, j, k, l))
 
 print(ans)
