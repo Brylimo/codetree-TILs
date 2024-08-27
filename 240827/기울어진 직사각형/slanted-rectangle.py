496a82dd67e39886b1e798f7563553548ca0497d
Chaejin <icj0103@gmail.com>
2024-08-27 18:07:33 +0900

    Title: 기울어진 직사각형, Time: 144ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240827/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,43 @@
+n = int(input())
+
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+ans = 0
+def draw_rect(x, y):
+    global ans
+    dx = [-1, -1, 1, 1]
+    dy = [1, -1, -1, 1]
+
+    check = [0, 0, 0, 0]
+    dir = 0
+
+    sum_val = 0
+    ax, ay = x, y
+
+    while True:
+        nx = ax + dx[dir]
+        ny = ay + dy[dir]
+
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            if check[dir] == 0:
+                break
+            dir += 1
+            if dir == 4:
+                break
+                
+            continue
+            
+        sum_val += grid[nx][ny]
+        check[dir] += 1
+
+        ax, ay = nx, ny
+
+    ans = max(ans, sum_val)
+
+for i in range(n):
+    for j in range(n):
+        draw_rect(i, j)
+
+print(ans)
