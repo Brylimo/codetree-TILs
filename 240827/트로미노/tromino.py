3726b9d0031d2217f21731a5722cb8fa2e8bfba4
Chaejin <icj0103@gmail.com>
2024-08-27 15:32:08 +0900

    Title: 트로미노, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240827/트로미노/tromino.py
@@ -0,0 +1,43 @@
+n, m = map(int, input().split())
+
+grid = []
+for i in range(n):
+    grid.append(list(map(int, input().split())))
+
+dx = [
+    [0, -1, -2],
+    [0, 0, 0],
+    [0, -1, -1],
+    [0, 0, -1],
+    [-1, -1, 0],
+    [0, 0, -1]
+]
+dy = [
+    [0, 0, 0],
+    [0, 1, 2],
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 1, 1]
+]
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        for k in range(6):
+            sum_val = 0
+            flag = True
+            for h in range(3):
+                nx = i + dx[k][h]
+                ny = j + dy[k][h]
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    flag = False
+                    break
+
+                sum_val += grid[nx][ny]
+
+            if flag:
+                ans = max(ans, sum_val)
+
+print(ans)
