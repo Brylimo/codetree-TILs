2af41167b4208b5ccc7f876d741d020f29621c91
Chaejin <icj0103@gmail.com>
2024-08-27 11:14:10 +0900

    Title: 수열의 순서 바꾸기, Time: 5082ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240827/수열의 순서 바꾸기/reorder-sequence.py
@@ -0,0 +1,34 @@
+import sys
+
+n = int(input())
+array = list(map(int, input().split()))
+right = list(range(1, n + 1))
+
+end = max(array)
+if len(array) == 2:
+    if array != right:
+        print(1)
+    else:
+        print(0)
+
+cnt = 0
+while True:
+    if right == array:
+        break
+
+    idx = 0
+    min_val = sys.maxsize
+    target = array.pop(0)
+    for i in range(2, n - 1):
+        if end == target:
+            idx = n - 1
+            break
+        
+        if min_val > array[i] - target:
+            min_val = array[i] - target
+            idx = i
+
+    array.insert(idx, target)
+    cnt += 1
+
+print(cnt)
