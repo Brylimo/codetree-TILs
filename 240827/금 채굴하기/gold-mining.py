6e017e32f33f807589a2e3a3c037ce110f52441f
Chaejin <icj0103@gmail.com>
2024-08-27 17:13:38 +0900

    Title: 금 채굴하기, Time: 251ms, Memory: 27MB, Status: Passed - CodeTree

--- 240827/금 채굴하기/gold-mining.py
+++ 240827/금 채굴하기/gold-mining.py
@@ -16,33 +16,30 @@ def draw_diamond(x, y):
 
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
-    queue.append((x, y, 1))
+    queue.append((x, y, 0))
 
     visited[x][y] = True
     value += grid[x][y]
 
-    cur_level = 0
-    cnt = 1
+    cur_level = -1
     while queue:
         ax, ay, level = queue.popleft()
 
         if level != cur_level:
-            cur_level = level
-            if cnt <= value * m:
+            if level ** 2 + (level + 1) ** 2 <= value * m:
                 ans = max(ans, value)
+            cur_level = level
 
         for i in range(4):
             nx = ax + dx[i]
             ny = ay + dy[i]
 
             if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                cnt += 1
                 continue
 
             if not visited[nx][ny]:
                 visited[nx][ny] = True
                 value += grid[nx][ny]
-                cnt += 1
 
                 queue.append((nx, ny, level + 1))
         
