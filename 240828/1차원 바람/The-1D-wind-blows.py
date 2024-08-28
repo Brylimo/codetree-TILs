b8f1dad04078e5c504086458b27bf32df37b6978
Chaejin <icj0103@gmail.com>
2024-08-28 11:24:12 +0900

    Title: 1차원 바람, Time: 266ms, Memory: 27MB, Status: Passed - CodeTree

--- 240828/1차원 바람/The-1D-wind-blows.py
+++ 240828/1차원 바람/The-1D-wind-blows.py
@@ -7,43 +7,42 @@ for _ in range(n):
     buildings.append(list(map(int, input().split())))
 
 queue = deque([])
-visited = [False] * n
 for i in range(q):
     r, d = input().split()
     r = int(r)
 
     queue.append((r, d, None))
 
-while queue:
-    ar, ad, afrom = queue.popleft()
+    while queue:
+        ar, ad, afrom = queue.popleft()
 
-    if ad == 'L':
-        temp = buildings[ar - 1][-1]
-        for i in range(m - 1, 0, -1):
-            buildings[ar - 1][i] = buildings[ar - 1][i - 1]
-        
-        buildings[ar - 1][0] = temp
+        if ad == 'L':
+            temp = buildings[ar - 1][-1]
+            for i in range(m - 1, 0, -1):
+                buildings[ar - 1][i] = buildings[ar - 1][i - 1]
+            
+            buildings[ar - 1][0] = temp
 
-        next_ad = 'R'
-    elif ad == 'R':
-        temp = buildings[ar - 1][0]
-        for i in range(m-1):
-            buildings[ar - 1][i] = buildings[ar - 1][i + 1]
+            next_ad = 'R'
+        elif ad == 'R':
+            temp = buildings[ar - 1][0]
+            for i in range(m-1):
+                buildings[ar - 1][i] = buildings[ar - 1][i + 1]
 
-        buildings[ar - 1][-1] = temp
-        
-        next_ad = 'L'
+            buildings[ar - 1][-1] = temp
+            
+            next_ad = 'L'
 
-    if ar - 2 >= 0 and afrom != ar - 2:
-        for j in range(m):
-            if buildings[ar - 1][j] == buildings[ar - 2][j]:
-                queue.append((ar - 1, next_ad, ar - 1))
-                break
-    if ar < n and afrom != ar:
-        for j in range(m):
-            if buildings[ar - 1][j] == buildings[ar][j]:
-                queue.append((ar + 1, next_ad, ar - 1))
-                break
+        if ar - 2 >= 0 and afrom != ar - 2:
+            for j in range(m):
+                if buildings[ar - 1][j] == buildings[ar - 2][j]:
+                    queue.append((ar - 1, next_ad, ar - 1))
+                    break
+        if ar < n and afrom != ar:
+            for j in range(m):
+                if buildings[ar - 1][j] == buildings[ar][j]:
+                    queue.append((ar + 1, next_ad, ar - 1))
+                    break
 
 for i in range(n):
     for j in range(m):
