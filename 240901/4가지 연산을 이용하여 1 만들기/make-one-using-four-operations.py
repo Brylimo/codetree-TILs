4e71904670ffc4dee51fe93a252c93d6421c07b9
Chaejin <icj0103@gmail.com>
2024-09-01 20:14:17 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 196ms, Memory: 40MB, Status: Passed - CodeTree

--- 240901/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240901/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -21,23 +21,25 @@ def bfs():
             break
 
         # 1을 뺀다
-        queue.append(num - 1)
-        visited[num - 1] = True
-        step[num - 1] = min(step[num - 1], step[num] + 1)
+        if num > 1 and not visited[num - 1]:
+            queue.append(num - 1)
+            visited[num - 1] = True
+            step[num - 1] = min(step[num - 1], step[num] + 1)
 
         # 1을 더한다
-        queue.append(num + 1)
-        visited[num + 1] = True
-        step[num + 1] = min(step[num + 1], step[num] + 1)
+        if num < MAX_NUM and not visited[num + 1]:
+            queue.append(num + 1)
+            visited[num + 1] = True
+            step[num + 1] = min(step[num + 1], step[num] + 1)
 
         # 2로 나눈다
-        if num % 2 == 0:
+        if num % 2 == 0 and not visited[num // 2]:
             queue.append(num // 2)
             visited[num // 2] = True
             step[num // 2] = min(step[num // 2], step[num] + 1)
 
         # 3으로 나눈다
-        if num % 3 == 0:
+        if num % 3 == 0 and not visited[num // 3]:
             queue.append(num // 3)
             visited[num // 3] = True
             step[num // 3] = min(step[num // 3], step[num] + 1)
