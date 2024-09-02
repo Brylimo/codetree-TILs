1b26a802b94f8ab7fdda275ab96dc48ec0bd98fb
Chaejin <icj0103@gmail.com>
2024-09-02 17:13:00 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 923ms, Memory: 66MB, Status: Passed - CodeTree

--- 240902/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240902/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,58 +1,42 @@
-from collections import deque
-
 n = int(input())
 grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 dp = [
-    [0] * n
-    for _ in range(n)
-]
-visited = [
-    [False] * n
+    [1] * n
     for _ in range(n)
 ]
 
-queue = deque()
-
-def move(x, y):
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    
-    dp[x][y] = grid[x][y]
-    visited[x][y] = True
-
-    queue.append((x, y))
-
-    while queue:
-        ax, ay = queue.popleft()
-
-        for i in range(4):
-            nx = ax + dx[i]
-            ny = ay + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-
-            if grid[nx][ny] > grid[ax][ay] and not visited[nx][ny]:
-                visited[nx][ny] = True
-                queue.append((nx, ny))
-
-ans = 0
+orders = []
 for i in range(n):
     for j in range(n):
-        move(i, j)
+        orders.append((grid[i][j], i, j))
 
-        cnt = 0
-        for i in range(n):
-            for j in range(n):
-                if visited[i][j]:
-                    cnt += 1
+orders.sort(reverse = True)
 
-                visited[i][j] = False
-                dp[i][j] = False
+def get_counts(x, y):
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
 
-        ans = max(ans, cnt)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+
+        if grid[nx][ny] < grid[x][y]:
+            dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+
+for order in orders:
+    num, x, y = order
+    get_counts(x, y)
+
+ans = max([
+    dp[i][j]
+    for i in range(n)
+    for j in range(n)
+])
 
 print(ans)
