ec6b7f47935cbc41854d7eae0e0e8b742457d5a5
Chaejin <icj0103@gmail.com>
2024-09-03 10:38:17 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 206ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240903/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -5,13 +5,15 @@ lines = [
 ]
 dp = [1] * n
 
+lines.sort(key = lambda x: x[1])
+
 for i in range(n):
     for j in range(i):
         xi1, xi2 = lines[i]
         xj1, xj2 = lines[j]
 
-        # 겹침
-        if xj2 < xj1 or xj2 < xi1:
+        # 겹치지 않음
+        if xj2 < xi1:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
