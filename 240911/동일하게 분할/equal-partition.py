ed52a053040ee11425946549281ef60316e162c2
Chaejin <icj0103@gmail.com>
2024-09-11 14:24:47 +0900

    Title: 동일하게 분할, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240911/동일하게 분할/equal-partition.py
@@ -0,0 +1,25 @@
+import sys
+INT_MIN = -sys.maxsize
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+total = sum(nums)
+dp = [INT_MIN] * (total + 1)
+dp[0] = 0
+
+for j in range(n):
+    for i in range(total, -1, -1):
+        if i >= nums[j]:
+            if dp[i - nums[j]] == INT_MIN:
+                continue
+            
+            dp[i] = 1
+
+for i in range(total + 1):
+    other = total - dp[i]
+    if other == i:
+        print("Yes")
+        sys.exit(0)
+        
+print("No")
