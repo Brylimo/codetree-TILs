9f74153765ebc9425f41e0d6909b4b478c15da16
Chaejin <icj0103@gmail.com>
2024-09-11 15:48:40 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 162ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240911/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -0,0 +1,13 @@
+import sys
+INT_MIN = -sys.maxsize
+
+n = int(input())
+array = list(map(int, input().split()))
+
+dp = [INT_MIN] * n
+dp[0] = array[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + array[i], array[i])
+
+print(max(dp))
