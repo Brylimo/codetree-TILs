c94f6058ae799ff1de9f37bf4db133dfdbeac65f
Chaejin <icj0103@gmail.com>
2024-09-12 22:09:43 +0900

    Title: 둘 중 하나 잘 고르기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240912/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 240912/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -1,10 +1,13 @@
+import sys
+INT_MIN = -sys.maxsize
+
 n = int(input())
 cards = [
     tuple(map(int, input().split()))
     for _ in range(2 * n)
 ]
 dp = [
-    [0] * (n + 1)
+    [INT_MIN] * (n + 1)
     for _ in range(2 * n + 1)
 ]
 dp[0][0] = 0
@@ -14,6 +17,15 @@ for i in range(1, 2 * n + 1):
 
 for i in range(1, 2 * n + 1):
     for j in range(1, n + 1):
-        dp[i][j] = max(dp[i - 1][j - 1] + cards[i - 1][0], dp[i - 1][j] + cards[i - 1][1])
+        if dp[i - 1][j - 1] != INT_MIN:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + cards[i - 1][0])
 
+        if dp[i - 1][j] != INT_MIN:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j] + cards[i - 1][1])
+
+'''
+for i in range(2 * n + 1):
+    print(dp[i])
+print()
+'''
 print(dp[2 * n][n])
