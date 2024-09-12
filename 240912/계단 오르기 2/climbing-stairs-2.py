6a18ee3b3fc58c39bff45b404b6279c7eac9960a
Chaejin <icj0103@gmail.com>
2024-09-12 11:20:32 +0900

    Title: 계단 오르기 2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240912/계단 오르기 2/climbing-stairs-2.py
+++ 240912/계단 오르기 2/climbing-stairs-2.py
@@ -13,11 +13,11 @@ dp[0][0] = 0
 for i in range(1, n + 1):
     if dp[i - 2][0] != INT_MIN:
         dp[i][0] = dp[i - 2][0] + stairs[i - 1]
-    if dp[i - 1][0] != INT_MIN or dp[i - 2][0] != INT_MIN:
-        dp[i][1] = max(dp[i - 1][0], dp[i - 2][0]) + stairs[i - 1]
-    if dp[i - 1][1] != INT_MIN or dp[i - 2][1] != INT_MIN: 
-        dp[i][2] = max(dp[i - 1][1], dp[i - 2][1]) + stairs[i - 1]
-    if dp[i - 1][2] != INT_MIN or dp[i - 2][2] != INT_MIN:
-        dp[i][3] = max(dp[i - 1][2], dp[i - 2][2]) + stairs[i - 1]
+    if dp[i - 1][0] != INT_MIN or dp[i - 2][1] != INT_MIN:
+        dp[i][1] = max(dp[i - 1][0], dp[i - 2][1]) + stairs[i - 1]
+    if dp[i - 1][1] != INT_MIN or dp[i - 2][2] != INT_MIN: 
+        dp[i][2] = max(dp[i - 1][1], dp[i - 2][2]) + stairs[i - 1]
+    if dp[i - 1][2] != INT_MIN or dp[i - 2][3] != INT_MIN:
+        dp[i][3] = max(dp[i - 1][2], dp[i - 2][3]) + stairs[i - 1]
 
 print(max(dp[n]))
