3ab4fb0696e222f299b7ad2f94308f49913a7342
Chaejin <icj0103@gmail.com>
2024-09-17 11:49:06 +0900

    Title: 테디의 여행 플래너, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240917/테디의 여행 플래너/teddys-travel-planner.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+n, q = map(int, input().split())
+cities = dict()
+
+array = list(input().split())
+pinned = array[0]
+
+for i in array:
+    cities[i] = Node(i)
+
+def connect(s, e):
+    if s is not None:
+        s.next = e
+    if e is not None:
+        e.prev = s
+
+def move_pin_to_right():
+    global pinned
+
+    if cities[pinned].next is not None:
+        pinned = cities[pinned].next.data
+
+def move_pin_to_left():
+    global pinned
+    
+    if cities[pinned].prev is not None:
+        pinned = cities[pinned].prev.data
+
+def pop_right():
+    if cities[pinned].next is not None:
+        target = cities[pinned].next
+
+        connect(target.prev, target.next)
+        target.prev, target.next = None, None
+
+def insert_city(a):
+    connect(a, cities[pinned].next)
+    connect(cities[pinned], a)
+
+for i in range(n - 1):
+    connect(cities[array[i]], cities[array[i + 1]])
+connect(cities[array[n - 1]], cities[array[0]])
+
+for _ in range(q):
+    query = list(input().split())
+
+    option = int(query[0])
+    if pinned == None:
+        continue
+
+    if option == 1:
+        move_pin_to_right()
+    elif option == 2:
+        move_pin_to_left()
+    elif option == 3:
+        pop_right()
+    elif option == 4:
+        a = query[1]
+        insert_city(Node(a))
+
+    node = cities[pinned]
+
+    if node is not None:
+        if node.prev is not None and node.next is not None and node.prev.data == node.next.data:
+            print(-1)
+        elif node.prev is None or node.next is None:
+            print(-1)
+        else:
+            if node.prev is not None:
+                print(node.prev.data, end=" ")
+
+            if node.next is not None:
+                print(node.next.data, end="")
+
+            print()
