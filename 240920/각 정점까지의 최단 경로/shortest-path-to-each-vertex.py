1cbcefda3054154f2015446ea005f0ed9be4e650
Chaejin <icj0103@gmail.com>
2024-09-20 22:17:29 +0900

    Title: 각 정점까지의 최단 경로, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240920/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,37 @@
+import sys
+import heapq
+INT_MAX = sys.maxsize
+
+n, m = map(int, input().split())
+k = int(input())
+
+graph = [[] for _ in range(n + 1)]
+dist = [INT_MAX] * (n + 1)
+
+for _ in range(m):
+    x, y, s = map(int, input().split())
+    graph[x].append((y, s))
+    graph[y].append((x, s))
+
+dist[k] = 0
+
+pq = []
+heapq.heappush(pq, (0, k))
+
+while pq:
+    min_dist, min_idx = heapq.heappop(pq)
+
+    if min_dist != dist[min_idx]:
+        continue
+
+    for i, s in graph[min_idx]:
+        if dist[i] > min_dist + s:
+            dist[i] = min_dist + s 
+            heapq.heappush(pq, (min_dist + s, i))
+
+print(0)
+for i in range(1, n + 1):
+    if i == k:
+        continue
+
+    print(dist[i])
