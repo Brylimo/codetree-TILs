841dd4dbd7bbc744e88b33fcd6cf6fd41fc30fde
Chaejin <icj0103@gmail.com>
2024-09-23 17:50:32 +0900

    Title: 가장 가까운 거리의 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240923/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.py
@@ -0,0 +1,42 @@
+import sys, heapq
+INT_MAX = sys.maxsize
+
+n, m = map(int, input().split())
+a, b, c = map(int, input().split())
+graph = [
+    [] for _ in range(n + 1)
+]
+
+dist = [INT_MAX] * (n + 1)
+for _ in range(m):
+    i, j, s = map(int, input().split())
+    graph[i].append((j, s))
+    graph[j].append((i, s))
+
+pq = []
+dist[a] = 0
+dist[b] = 0
+dist[c] = 0
+
+heapq.heappush(pq, (0, a))
+heapq.heappush(pq, (0, b))
+heapq.heappush(pq, (0, c))
+
+while pq:
+    min_dist, min_idx = heapq.heappop(pq)
+
+    if min_dist != dist[min_idx]:
+        continue
+
+    for target_idx, target_dist in graph[min_idx]:
+        cost = dist[min_idx] + target_dist
+
+        if dist[target_idx] > cost:
+            dist[target_idx] = cost
+            heapq.heappush(pq, (target_idx, cost))
+
+print(max([
+    dist[i]
+    for i in range(1, n + 1)
+    if dist[i] != INT_MAX
+]))
