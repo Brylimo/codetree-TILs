de44f514ecc70a067dff4fa9457fad6e3503f4c2
Chaejin <icj0103@gmail.com>
2024-09-25 17:54:01 +0900

    Title: 트리 파악, Time: 829ms, Memory: 138MB, Status: Passed - CodeTree

--- 240925/트리 파악/identifying-the-tree.py
+++ 240925/트리 파악/identifying-the-tree.py
@@ -1,54 +1,56 @@
+import sys
+sys.setrecursionlimit(100000)
+
+# 변수 선언 및 입력:
 n = int(input())
-
-root = 1
-graph = [[] for _ in range(n + 1)]
-parent = [-1] * (n + 1)
+edges = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
-piece = [[] for _ in range(n + 1)]
+depth = [0] * (n + 1)
 
+# 리프노드 깊이의 총합
+ans = 0
+
+# n - 1개의 간선 정보를 입력받습니다.
 for _ in range(n - 1):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    x, y = tuple(map(int, input().split()))
 
-def traverse(x):
-    global cnt
+    # 간선 정보를 인접리스트에 넣어줍니다.
+    edges[x].append(y)
+    edges[y].append(x)
 
+
+# DFS를 통해 리프노드와 리프노드의 깊이를 탐색합니다.
+def dfs(x):
+    global ans
+    
     is_leaf = True
 
-    for i in graph[x]:
-        if not visited[i]:
-            visited[i] = True
+    for y in edges[x]:
+        # 이미 방문한 노드는 스킵합니다.
+        if visited[y]: 
+            continue
 
-            parent[i] = x
-            traverse(i)
-            is_leaf = False
+        visited[y] = True
 
-    if is_leaf:
-        piece[x].append(1)
+        # 하나라도 자식 노드가 있다면 리프 노드가 아닙니다.
+        is_leaf = False
+        
+        # root로부터의 거리를 갱신합니다.
+        depth[y] = depth[x] + 1
 
-visited[root] = True
-traverse(root)
+        dfs(y)
 
-flag = 1
-while True:
-    moved = False
+    # 리프노드라면, 해당 노드의 깊이를 더합니다.
+    if is_leaf: 
+        ans += depth[x]
 
-    for i in range(1, n + 1):
-        if piece[i]: # 장기말이 있다면
-            p = sum(piece[i])
-            piece[i] = []
-            piece[parent[i]].append(p)
-            flag += p
-            moved = True
 
-    if piece[root]:
-        piece[root] = []
+# DFS를 통해 리프노드와 리프노드의 깊이를 탐색합니다.
+visited[1] = True
+dfs(1)
 
-    if not moved:
-        break
-
-if flag % 2 == 0:
+# 모든 리프노드의 깊이의 합이 짝수인지 홀수인지 판단해 정답을 출력합니다.
+if ans % 2 == 1: 
     print(1)
 else:
     print(0)
