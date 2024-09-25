77b76477503be3cd1b3d83cde4c8a79cb9a67b51
Chaejin <icj0103@gmail.com>
2024-09-25 17:00:58 +0900

    Title: 트리 정점 거리, Time: 199ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240925/트리 정점 거리/node-distance.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+step = [0] * (n + 1)
+for _ in range(n - 1):
+    x, y, s = map(int, input().split())
+    graph[x].append((y, s))
+    graph[y].append((x, s))
+
+def traverse(x, y):
+    if x == y:
+        return
+
+    for i, s in graph[x]:
+        if not visited[i]:
+            visited[i] = True
+            step[i] = step[x] + s
+            traverse(i, s)
+
+for i in range(m):
+    x, y = map(int, input().split())
+
+    for j in range(n + 1):
+        step[j] = 0
+        visited[j] = False
+
+    ans = 0
+    visited[x] = True
+    traverse(x, y)
+
+    ans += step[y]
+
+    print(ans)
