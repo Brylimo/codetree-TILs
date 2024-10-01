f51a8179bc07c9207d07567487f555bd1473656d
Chaejin <icj0103@gmail.com>
2024-10-01 15:45:07 +0900

    Title: 이상한 윷놀이, Time: 242ms, Memory: 29MB, Status: Passed - CodeTree

--- 241001/이상한 윷놀이/odd-woodstick-game.py
+++ 241001/이상한 윷놀이/odd-woodstick-game.py
@@ -31,8 +31,8 @@ def in_range(x, y):
 def end():
     flag = any([
         True
-        for i in range(n)
-        for j in range(n)
+        for i in range(1, n + 1)
+        for j in range(1, n + 1)
         if len(smap[i][j]) >= 4
     ])
 
