c8ff77b2b7d40859dece8a66bfd15d7fa4f86ca3
Chaejin <icj0103@gmail.com>
2024-10-02 22:26:07 +0900

    Title: 노드의 정수 계산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241002/노드의 정수 계산/calculating-an-integer-for-a-node.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+parents = [-1] * (n + 1)
+visited = [False] * (n + 1)
+
+d = [0] * (n + 1)
+array = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+for i in range(2, n + 1):
+    t, a, p = map(int, input().split())
+
+    if t == 1:
+        array[i] = a
+    else:
+        array[i] = -a
+
+    parents[i] = p
+    graph[i].append(p)
+    graph[p].append(i)
+
+def dfs(x):
+    for i in graph[x]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(i)
+
+    d[x] = array[x]
+    for i in graph[x]:
+        if parents[i] == x:
+            if d[i] > 0:
+                d[x] += d[i]
+
+visited[1] = True
+dfs(1)
+
+print(d[1])
