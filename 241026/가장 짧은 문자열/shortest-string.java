5c750e6991f21423e54850c665c0dff4df5ca2a4
Chaejin <icj0103@gmail.com>
2024-10-26 00:15:45 +0900

    Title: 가장 짧은 문자열, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 241026/가장 짧은 문자열/shortest-string.java
+++ 241026/가장 짧은 문자열/shortest-string.java
@@ -9,17 +9,17 @@ public class Main {
         int b = sc.next().length();
         int c = sc.next().length();
 
-        if (a > b && b > c) {
+        if (a >= b && b >= c) {
             System.out.println(a - c);
-        } else if (a > c && c > b) {
+        } else if (a >= c && c >= b) {
             System.out.println(a - b);
-        } else if (b > a && a > c) {
+        } else if (b >= a && a >= c) {
             System.out.println(b - c);
-        } else if (b > c && c > a) {
+        } else if (b >= c && c >= a) {
             System.out.println(b - a);
-        } else if (c > a && a > b) {
+        } else if (c >= a && a >= b) {
             System.out.println(c - b);
-        } else if (c > b && b > a) {
+        } else if (c >= b && b >= a) {
             System.out.println(c - a);
         }
     }
