8a993f9a74f2fa1c481df436a3a33d6e42827bc9
Chaejin <icj0103@gmail.com>
2024-10-26 14:22:19 +0900

    Title: 정수만 추출하기, Time: 117ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241026/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String first = "0", second = "0";
+        for (int i = 0; i < a.length(); i++) {
+            if (!('0' <= a.charAt(i) && a.charAt(i) <= '9')) {
+                first = a.substring(0, i);
+                break;
+            }
+        }
+
+        for (int i = 0; i < a.length(); i++) {
+            if (!('0' <= a.charAt(i) && a.charAt(i) <= '9')) {
+                second = b.substring(0, i);
+                break;
+            }
+        }
+
+        System.out.println(Integer.parseInt(first) + Integer.parseInt(second));
+    
+    }
+}
