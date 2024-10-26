99cd331c912637fc43b3748e71a577e3c96d523e
Chaejin <icj0103@gmail.com>
2024-10-26 14:11:01 +0900

    Title: 대문자로 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241026/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (('a' <= str.charAt(i) && str.charAt(i) <= 'z')) {
+                System.out.print((char)(str.charAt(i) - 'a' + 'A'));
+            } else if (('A' <= str.charAt(i) && str.charAt(i) <= 'Z')) {
+                System.out.print(str.charAt(i));
+            }
+        }
+    
+    }
+}
