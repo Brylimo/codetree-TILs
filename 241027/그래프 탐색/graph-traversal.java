76f51865da8eb2fcac571b95d82c272f1b2ce3e2
Chaejin <icj0103@gmail.com>
2024-10-27 16:57:47 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241027/그래프 탐색/graph-traversal.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 1000;
+    public static int cnt;
+    public static boolean[] visited = new boolean[MAX_N];
+    public static ArrayList<Integer>[] graph = new ArrayList[MAX_N + 1];
+    
+    public static void dfs(int vertex) {
+        for (int i = 0; i < graph[vertex].size(); i++) {
+            int current = graph[vertex].get(i);
+            if (!visited[current]) {
+                visited[current] = true;
+                cnt += 1;
+                dfs(current);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < MAX_N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        while (m-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            graph[x].add(y);
+            graph[y].add(x); 
+        }
+
+        visited[1] = true;
+        dfs(1);
+
+        System.out.println(cnt);
+    }
+}
