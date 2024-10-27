f187d1cdefa360dab4406158578ad77e2673039c
Chaejin <icj0103@gmail.com>
2024-10-27 14:39:09 +0900

    Title: 구간 중 최대 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241027/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_R = 100;
+    public static int[] arr = new int[MAX_R];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int maxVal = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+
+            maxVal = Math.max(maxVal, sum);
+        }
+
+        System.out.println(maxVal);
+    }
+}
