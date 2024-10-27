188a3304cd508e60db45aa591b49220bae4b30c6
Chaejin <icj0103@gmail.com>
2024-10-27 14:40:41 +0900

    Title: 구간 중 최대 합, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/구간 중 최대 합/max-sum-of-subarray.java
+++ 241027/구간 중 최대 합/max-sum-of-subarray.java
@@ -17,7 +17,7 @@ public class Main {
         }
 
         int maxVal = INT_MIN;
-        for (int i = 0; i < n - k; i++) {
+        for (int i = 0; i < n - k + 1; i++) {
             int sum = 0;
             for (int j = i; j < i + k; j++) {
                 sum += arr[j];
