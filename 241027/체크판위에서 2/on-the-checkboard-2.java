bb8f98f9a6beb4c33c5dd05171d8f896a62d30c5
Chaejin <icj0103@gmail.com>
2024-10-27 14:24:52 +0900

    Title: 체크판위에서 2, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/체크판위에서 2/on-the-checkboard-2.java
+++ 241027/체크판위에서 2/on-the-checkboard-2.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static char[][] grid;
-    public static int r, c;
+    public static int r, c, ans;
 
-    public static int jump(char current, int x, int y, int score) {
-        if (x == r - 1 || y == c - 1) return score;
+    public static boolean inRange(int x, int y) {
+        return (0 <= x && x < r && 0 <= y && y < c);
+    }
+
+    public static void jump(char current, int x, int y, int score) {
+        if (x == r - 1 && y == c - 1) {
+            if (score - 1 == 2) ans += 1;
+            return;
+        }
 
         for (int a = x + 1; a < r; a++) {
             for (int b = y + 1; b < c; b++) {
                 char target = grid[a][b];
 
                 if (current != target) {
-                    return jump(target, a, b, score + 1);
+                    jump(target, a, b, score + 1);
                 }
             }
         }
 
-        return score;
     }
 
     public static void main(String[] args) {
@@ -34,16 +40,8 @@ public class Main {
             }
         }
 
-        int ans = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                char current = grid[i][j];
-
-                int count = jump(current, i, j, 0);
-
-                if (count == 3) ans += 1;
-            }
-        }
+        char current = grid[0][0];
+        jump(current, 0, 0, 0);
     
         System.out.println(ans);
     }
