6fe97d3283db8af4bf70ef4e8cbec71577d20251
Chaejin <icj0103@gmail.com>
2024-10-27 16:16:57 +0900

    Title: 크기가 n인 순열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241027/크기가 n인 순열/n-permutation.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 8;
+    public static int n;
+
+    public static boolean[] visited = new boolean[MAX_N];
+    public static ArrayList<Integer> candidates = new ArrayList<>();
+
+    public static void calculate(int current) {
+        if (current == n) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(candidates.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            candidates.add(i);
+
+            calculate(current + 1);
+
+            candidates.remove(candidates.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        
+        calculate(0);
+    }
+}
