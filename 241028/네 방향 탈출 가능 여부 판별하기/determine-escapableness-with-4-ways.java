8a1275b08b1f40085716612e2c35cd744803fd2a
Chaejin <icj0103@gmail.com>
2024-10-28 11:10:28 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241028/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_N = 100;
+    
+    public static int n, m;
+    public static int[][] graph = new int[MAX_N][MAX_N];
+    public static boolean[][] visited = new boolean[MAX_N][MAX_N];
+
+    public static int[] dx = new int[]{-1, 0, 1, 0};
+    public static int[] dy = new int[]{0, 1, 0, -1};
+
+    public static Queue<Point> queue = new LinkedList<>();
+
+    public static boolean inRange(int x, int y) {
+        return (0 <= x && x < n && 0 <= y && y < m);
+    }
+
+    public static void bfs(int x, int y) {
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
+
+                if (!inRange(nx, ny)) continue;
+
+                if (!visited[nx][ny] && graph[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.add(new Point(nx, ny));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited[0][0] = true;
+        queue.add(new Point(0, 0));
+        bfs(0, 0);
+
+        if (visited[n - 1][n - 1]) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
