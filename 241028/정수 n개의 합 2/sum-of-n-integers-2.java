d0ed8d1f3ce5c83fc6f31218f514eb9eaa31ff73
Chaejin <icj0103@gmail.com>
2024-10-28 21:41:25 +0900

    Title: 정수 n개의 합 2, Time: 222ms, Memory: 20MB, Status: Passed - CodeTree

---
+++ 241028/정수 n개의 합 2/sum-of-n-integers-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] prefixSum = new int[n];
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        int maxVal = INT_MIN;
+        for (int i = 0; i < n - k; i++) {
+            maxVal = Math.max(maxVal, prefixSum[i + k] - prefixSum[i]);
+        }
+
+        System.out.println(maxVal);
+    }
+}
