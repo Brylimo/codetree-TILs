2de9dc53efbaf815af125667ad65a17896c8b4ef
Chaejin <icj0103@gmail.com>
2024-10-29 11:19:34 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 533ms, Memory: 39MB, Status: Passed - CodeTree

--- 241029/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 241029/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_N = 100000;
+    public static final int MAX_N = 1000000;
 
     public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
