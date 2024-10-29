9cd39ef91aded58a434ea4ecd0b0cfcf356c1eac
Chaejin <icj0103@gmail.com>
2024-10-29 11:17:45 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241029/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[MAX_N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            arr[x] = 1;
+        }
+
+        int[] prefixSum = new int[MAX_N + 1];
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < MAX_N + 1; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        } 
+
+        while (q-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(prefixSum[b] - prefixSum[a] + arr[a]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
