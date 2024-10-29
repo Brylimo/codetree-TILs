0ab1470b376265609f9d9f05d1516228a4804926
Chaejin <icj0103@gmail.com>
2024-10-29 16:26:49 +0900

    Title: 최대 감소 부분 수열, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241029/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxVal = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            maxVal = Math.max(maxVal, dp[i]);
+        }
+
+        System.out.println(maxVal);
+    }
+}
