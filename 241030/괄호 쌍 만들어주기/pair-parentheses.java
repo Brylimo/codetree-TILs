d4085c520df1767ed69e86523ce8f963db20264b
Chaejin <icj0103@gmail.com>
2024-10-30 14:40:31 +0900

    Title: 괄호 쌍 만들어주기, Time: 98ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/괄호 쌍 만들어주기/pair-parentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] array = br.readLine().toCharArray();
+
+        int len = array.length;
+        int[] R = new int[len];
+
+        if (array[len - 1] == ')')
+            R[len - 1] = 1;
+        
+        for (int i = len - 2; i >= 0; i--) {
+            if (array[i] == ')')
+                R[i] = R[i + 1] + 1;
+            else
+                R[i] = R[i + 1];
+        }
+
+        //System.out.println(Arrays.toString(R));
+
+        int cnt = 0;
+        for (int i = 0; i < len - 1; i++) {
+            if (array[i] == '(' && array[i + 1] == '(') {
+                cnt += R[i + 1] / 2;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
