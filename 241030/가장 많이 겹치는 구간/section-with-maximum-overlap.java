b234aa876c97ac0fb6ad1e002f8866b80392be5f
Chaejin <icj0103@gmail.com>
2024-10-30 11:51:54 +0900

    Title: 가장 많이 겹치는 구간, Time: 359ms, Memory: 22MB, Status: Passed - CodeTree

--- 241030/가장 많이 겹치는 구간/section-with-maximum-overlap.java
+++ 241030/가장 많이 겹치는 구간/section-with-maximum-overlap.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_N = 100001;
+    public static final int MAX_N = 200001;
 
     public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
