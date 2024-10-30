5c2cf5feb86e82758683389d21d9c2a8944c25e6
Chaejin <icj0103@gmail.com>
2024-10-30 14:16:39 +0900

    Title: 가장 많이 겹치는 구간 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241030/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] line = new int[MAX_N + 1];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+
+            line[x1] = 1;
+            line[x2] = -1;
+        }
+
+        int ans = 0;
+        int sum = 0;
+        for (int i = 0; i <= MAX_N; i++) {
+            sum += line[i];
+            ans = Math.max(ans, sum);
+        }
+
+        System.out.println(ans);
+    }
+}
