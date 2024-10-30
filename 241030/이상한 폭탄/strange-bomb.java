f09bca57d78aabbdb78d9c6c92e101e2b7310ff6
Chaejin <icj0103@gmail.com>
2024-10-30 15:24:35 +0900

    Title: 이상한 폭탄, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241030/이상한 폭탄/strange-bomb.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        ArrayList<Integer>[] R = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            R[i] = new ArrayList<>();
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + 1 < n) {
+                R[i].add(arr[i + 1]);
+            }
+            if (i + 2 < n) {
+                R[i].add(arr[i + 2]);
+            }
+            if (i + 3 < n) {
+                R[i].add(arr[i + 3]);
+            }
+            if (i - 1 >= 0) {
+                R[i].add(arr[i - 1]);
+            }
+            if (i - 2 >= 0) {
+                R[i].add(arr[i - 2]);
+            }
+            if (i - 3 >= 0) {
+                R[i].add(arr[i - 3]);
+            }
+        }
+
+        int maxNum = -1;
+        for (int i = 0; i < n; i++) {
+            if (R[i].indexOf(arr[i]) != -1)
+                maxNum = Math.max(maxNum, arr[i]);
+        }
+
+        System.out.println(maxNum);
+    }
+}
