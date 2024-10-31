345d27c756573d99c97f900e3d845faa7ca9a9d7
Chaejin <icj0103@gmail.com>
2024-10-31 15:33:40 +0900

    Title: 가장 먼저 나오는 숫자, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241031/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr;
+
+    public static int lowerBound(int x) {
+        int left = 0;
+        int right = n - 1;
+
+        int idx = n;
+        int target = x;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] >= target) {
+                right = mid - 1;
+
+                idx = Math.min(idx, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return idx + 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            int ans = lowerBound(x);
+
+            if (ans == n || (ans - 1 < n && arr[ans - 1] != x)) System.out.println(-1);
+            else System.out.println(ans);
+        }
+    }
+}
