8694fb4ba741517445b0a3d579987e3af5429779
Chaejin <icj0103@gmail.com>
2024-11-16 14:16:34 +0900

    Title: 최대 숫자 만들기, Time: 601ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241116/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+
+        String ans = "";
+        for (String str : arr) {
+            ans += str;
+        }
+
+        System.out.println(ans);
+    }
+}
