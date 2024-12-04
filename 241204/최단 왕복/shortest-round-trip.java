ffd42886457f41522b744d00a46d334622adf452
Chaejin <icj0103@gmail.com>
2024-12-04 19:31:49 +0900

    Title: 최단 왕복, Time: 93ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241204/최단 왕복/shortest-round-trip.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        long[][] graph = new long[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = Integer.parseInt(st.nextToken());
+
+            graph[i][i] = 0;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        long ans = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                long sum = graph[i][j] + graph[j][i];
+
+                ans = Math.min(ans, sum);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
